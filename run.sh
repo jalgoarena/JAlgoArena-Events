d31b60716116d35d6fd9236474e8382da7252fa5
Jacek Spólnik <jacek.spolnik@gmail.com>
2018-04-10 22:05:12 +0200

    configure travis

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-PORT=5005 nohup java -jar jalgoarena-events-*.jar &
+BOOTSTRAP_SERVERS="localhost:9092,localhost:9093,localhost:9094" PORT=5005 nohup java -jar jalgoarena-events-*.jar &
