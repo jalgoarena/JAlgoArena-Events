0d9128c41294e7a1629394097a2f23768136538c
Jacek Spólnik <jacek.spolnik@gmail.com>
2018-04-29 10:39:34 +0200

    - remove Docker & Heroku related configs - use pm2 for deploying and managing microservices

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM openjdk:8
-
-WORKDIR /app
-ADD build/libs/jalgoarena-events-*.jar /app/
-
-ENV BOOTSTRAP_SERVERS=kafka1:9092,kafka2:9093,kafka3:9094
-EXPOSE 5005
-
-CMD java -Dserver.port=5005 -jar /app/jalgoarena-events-*.jar
---
+++ pm2.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    apps: [
+        {
+            name: 'events',
+            args: [
+                "-jar",
+                "build/libs/jalgoarena-events-2.0.0-SNAPSHOT.jar"
+            ],
+            script: 'java',
+            env: {
+                PORT: 5005,
+                BOOTSTRAP_SERVERS: 'localhost:9092,localhost:9093,localhost:9094'
+            }
+        }
+    ]
+};
