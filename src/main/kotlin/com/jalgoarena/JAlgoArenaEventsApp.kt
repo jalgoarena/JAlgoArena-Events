3589aa8e503f447e9947e1e782b69dc8e6c430f6
Jacek Spólnik <jacek.spolnik@gmail.com>
2018-06-09 14:25:23 +0200

    - add events to Consul (Node.JS later can query Consul for API Gateway and WS Service) - remove heroku config

--- src/main/kotlin/com/jalgoarena/JAlgoArenaEventsApp.kt
+++ src/main/kotlin/com/jalgoarena/JAlgoArenaEventsApp.kt
@@ -2,10 +2,12 @@ package com.jalgoarena
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.kafka.annotation.EnableKafka
 
 @SpringBootApplication
 @EnableKafka
+@EnableDiscoveryClient
 open class JAlgoArenaEventsApp
 
 fun main(args: Array<String>) {
