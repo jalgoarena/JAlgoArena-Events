01546631ab4da5cca4f5ff276ce54be3eb57d333
Jacek Spólnik <jacek.spolnik@gmail.com>
2018-08-10 09:51:24 +0200

    Events improvements - revert back to original approach, just with some batching improvements

--- src/main/kotlin/com/jalgoarena/AppConfiguration.kt
+++ src/main/kotlin/com/jalgoarena/AppConfiguration.kt
@@ -1,22 +1,10 @@
 package com.jalgoarena
 
-import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.springframework.context.annotation.Configuration
 import org.springframework.messaging.simp.config.MessageBrokerRegistry
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
-import org.springframework.kafka.listener.BatchLoggingErrorHandler
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory
-import org.springframework.kafka.core.ConsumerFactory
-import org.apache.kafka.clients.consumer.ConsumerConfig.MAX_POLL_RECORDS_CONFIG
-import org.apache.kafka.clients.consumer.ConsumerConfig.GROUP_ID_CONFIG
-import org.apache.kafka.clients.consumer.ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG
-import org.apache.kafka.clients.consumer.ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG
-import org.apache.kafka.common.serialization.StringDeserializer
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.context.annotation.Bean
 
 
 @Configuration
@@ -32,33 +20,4 @@ open class AppConfiguration : WebSocketMessageBrokerConfigurer {
                 .setAllowedOrigins("*")
                 .withSockJS()
     }
-
-    @Value("\${spring.kafka.bootstrap-servers:localhost:9092,localhost:9093,localhost:9094}")
-    private lateinit var bootstrapServers: String
-
-    @Value("\${spring.kafka.consumer.group-id:events}")
-    private lateinit var groupId: String
-
-    @Bean
-    open fun consumerConfigs() =
-            mapOf(
-                    ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to bootstrapServers,
-                    KEY_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.java,
-                    VALUE_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.java,
-                    GROUP_ID_CONFIG to groupId,
-                    MAX_POLL_RECORDS_CONFIG to "10000",
-                    ConsumerConfig.FETCH_MAX_WAIT_MS_CONFIG to "1000"
-            )
-
-    @Bean
-    open fun consumerFactory(): ConsumerFactory<String, String> =
-            DefaultKafkaConsumerFactory(consumerConfigs())
-
-    @Bean
-    open fun kafkaListenerContainerFactory() =
-            ConcurrentKafkaListenerContainerFactory<String, String>().apply {
-                consumerFactory = consumerFactory()
-                isBatchListener = true
-                containerProperties.setBatchErrorHandler(BatchLoggingErrorHandler())
-            }
 }
