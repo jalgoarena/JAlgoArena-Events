327a86e6f99ae430fab8828d8e6b017bb7fbf173
Jacek Spólnik <jacek.spolnik@gmail.com>
2018-08-10 08:54:47 +0200

    Events improvements - batch events based on max number of them (10k) or time (1s)

--- src/main/kotlin/com/jalgoarena/EventsController.kt
+++ src/main/kotlin/com/jalgoarena/EventsController.kt
@@ -1,5 +1,9 @@
 package com.jalgoarena
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.jalgoarena.GenericEvent.Companion.REFRESH_RANKING_EVENT
+import com.jalgoarena.GenericEvent.Companion.REFRESH_SUBMISSIONS_EVENT
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.kafka.annotation.KafkaListener
@@ -15,8 +19,29 @@ class EventsController {
     private lateinit var messagingTemplate: SimpMessagingTemplate
 
     @KafkaListener(topics = ["events"])
-    fun handleEvent(event: String) {
-        logger.info("Publishing {} event", event)
-        messagingTemplate.convertAndSend("/topic/events", event)
+    fun handleEvent(events: List<String>) {
+        val genericEvents = transformToEvents(events)
+
+        genericEvents[REFRESH_SUBMISSIONS_EVENT]?.forEach {
+            logger.info("Publishing {} event", it)
+            messagingTemplate.convertAndSend("/topic/events", OBJECT_MAPPER.writeValueAsString(it))
+        }
+
+        genericEvents[REFRESH_RANKING_EVENT]?.run {
+            logger.info("Publishing {} event", REFRESH_RANKING_EVENT)
+            messagingTemplate.convertAndSend("/topic/events", REFRESH_RANKING_EVENT)
+        }
+    }
+
+    private fun transformToEvents(events: List<String>): Map<String, List<GenericEvent>> {
+        return events.map { OBJECT_MAPPER.readValue<GenericEvent>(it) }
+                .groupBy { it.type }
+    }
+
+    companion object {
+        private val OBJECT_MAPPER = jacksonObjectMapper()
+        private val REFRESH_RANKING_EVENT = """{
+    "type": "${GenericEvent.REFRESH_RANKING_EVENT}"
+}""".trimIndent()
     }
 }
